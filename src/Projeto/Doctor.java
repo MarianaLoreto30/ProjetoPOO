161aba4d6880cf3d3581fddf835e44748dd502f2
MarianaLoreto30 <56931097+MarianaLoreto30@users.noreply.github.com>
2019-11-12 13:59:17 +0000

    ...

--- src/Projeto/Doctor.java
+++ src/Projeto/Doctor.java
@@ -1,6 +1,18 @@
 package Projeto;
 
-public class Doctor extends  Scholar{
+import java.util.Date;
 
+public class Doctor extends  Scholar{
+    public Doctor(Date startDate, Date finalDate) {
+        super(startDate, finalDate);
+    }
+
+    public Doctor(String name, String eMail, Date startDate, Date finalDate, String cost) {
+        super(name, eMail, startDate, finalDate, cost);
+    }
+
+    public int calcCost(){
+        return 1000;
+    }
 
 }
